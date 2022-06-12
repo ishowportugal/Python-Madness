1225c0aac02bc69902087240edaf313467227402
Amiyaz on GitHub <amiyazishere@gmail.com>
2022-06-12 14:50:19 +0800

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        SomeRandomProject:D
+# Purpose: idk
+#
+# Author:      HappyOne
+#
+# Created:     15/05/2022
+# Copyright:   (c) AmiyazTheAmazing 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+fac=lambda n: fac(n-1)*n if n>1 else n
+
