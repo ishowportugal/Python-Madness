463283623eb71733c3e51035d06fa378d2f6f115
Amiyaz <amiyazishere@gmail.com>
2022-07-13 09:36:43 +0800

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,3 +1,3 @@
-# credits to @lomnom
+# credits to lomnom
 py
 num_grid=lambda l: [['#' if i=='#' else str(sum([len(l)>(y+Y)>=0 and len(l)>(x+X)>=0 and l[(y+Y)][(x+X)]=='#'for X,Y in((0,1),(1,0),(-1,0),(0,-1),(1,1),(-1,-1),(-1,1),(1,-1))]))for x,i in enumerate(r)]for y,r in enumerate(l)]
